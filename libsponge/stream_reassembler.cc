33b5f88d96b7fd1665455f2fa19be114aefdc986
wangdh15 <13020023780@163.com>
2022-06-16 17:19:17 +0800

    pass all lab1

--- libsponge/stream_reassembler.cc
+++ libsponge/stream_reassembler.cc
@@ -1,4 +1,5 @@
 #include "stream_reassembler.hh"
+#include <cassert>
 
 // Dummy implementation of a stream reassembler.
 
@@ -12,15 +13,85 @@ void DUMMY_CODE(Targs &&... /* unused */) {}
 
 using namespace std;
 
-StreamReassembler::StreamReassembler(const size_t capacity) : _output(capacity), _capacity(capacity) {}
+StreamReassembler::StreamReassembler(const size_t capacity) :
+    _output(capacity), _capacity(capacity), all_data_(), range_begin_(0),
+    unassembled_bytes_(0), receive_eof_(false), eof_index_(0) {}
 
 //! \details This function accepts a substring (aka a segment) of bytes,
 //! possibly out-of-order, from the logical stream, and assembles any newly
 //! contiguous substrings and writes them into the output stream in order.
 void StreamReassembler::push_substring(const string &data, const size_t index, const bool eof) {
-    DUMMY_CODE(data, index, eof);
+    if (!receive_eof_ && eof) {
+        receive_eof_ = true;
+        eof_index_ = index + data.size();
+        if (range_begin_ == eof_index_) {
+            _output.end_input();
+        }
+    }
+    // firstly, delete the string outof range
+    size_t len_byteStream = _output.buffer_size();
+    size_t range_end = _capacity - len_byteStream + range_begin_;
+    // without overlap
+    if (index >= range_end || index + data.size() <= range_begin_) {
+        return;
+    }
+    // get the data in the range
+    std::string data_in_range = data;
+    size_t l_in_range = index;
+    if (index < range_begin_) {
+        data_in_range = data_in_range.substr(range_begin_ - index, data_in_range.size());
+        l_in_range = range_begin_;
+    }
+    if (l_in_range + data_in_range.size() > range_end) {
+        data_in_range = data_in_range.substr(0, range_end - l_in_range);
+    }
+    if (data_in_range.size() == 0) {
+        return;
+    }
+    size_t r_in_range = l_in_range + data_in_range.size() - 1;
+    auto iter = all_data_.lower_bound({l_in_range, 0});
+    // overlap with left range
+    if (iter != all_data_.begin()) {
+        --iter;
+        if (iter->first.second + 1 >= l_in_range) {
+            if (iter->first.second >= r_in_range) {
+                data_in_range = iter->second;
+            } else {
+                data_in_range = iter->second + data_in_range.substr(iter->first.second + 1 - l_in_range, data_in_range.size());
+            }
+            l_in_range = iter->first.first;
+            r_in_range = max(iter->first.second, r_in_range);
+            unassembled_bytes_ -= iter->second.size();
+            iter = all_data_.erase(iter);
+        } else {
+            ++iter;
+        }
+    }
+    // check other range
+    while (iter != all_data_.end() && r_in_range + 1 >= iter->first.first) {
+        if (r_in_range < iter->first.second) {
+            data_in_range += iter->second.substr(r_in_range + 1 - iter->first.first, iter->second.size());
+            r_in_range = iter->first.second;
+        }
+        unassembled_bytes_ -= iter->second.size();
+        iter = all_data_.erase(iter);
+    }
+    assert(data_in_range.size() == r_in_range - l_in_range + 1);
+    all_data_[{l_in_range, r_in_range}] = data_in_range;
+    unassembled_bytes_ += data_in_range.size();
+    if (all_data_.begin()->first.first == range_begin_) {
+        _output.write(all_data_.begin()->second);
+        unassembled_bytes_ -= all_data_.begin()->second.size();
+        range_begin_ = all_data_.begin()->first.second + 1;
+        all_data_.erase(all_data_.begin());
+    }
+    if (receive_eof_ && range_begin_ == eof_index_) {
+        _output.end_input();
+    }
 }
 
-size_t StreamReassembler::unassembled_bytes() const { return {}; }
+size_t StreamReassembler::unassembled_bytes() const {
+    return unassembled_bytes_;
+ }
 
-bool StreamReassembler::empty() const { return {}; }
+bool StreamReassembler::empty() const { return unassembled_bytes() == 0; }
--- writeups/lab1.md
+++ writeups/lab1.md
@@ -12,7 +12,28 @@ I would like to thank/reward these classmates for their help: [list sunetids her
 This lab took me about [n] hours to do. I [did/did not] attend the lab session.
 
 Program Structure and Design of the StreamReassembler:
-[]
+
+
+1. 可以将问题抽象为实现一个维护区间的数据结构，支持如下的操作：
+    - 插入一个区间。（对重复的区间进行去重）
+    - 将区间的一开始连续的部分传递给另一个对象
+    - 可维护的整个区间的长度是一个有限的值。
+
+那么可以使用经典的树形结构来进行维护。树的每个节点存储三个内容：
+    - 区间左端点
+    - 区间右端点
+    - 字符串
+
+当一个新的区间加入的时候：
+1. 首先根据capacity判断可以容纳的字符串的最大范围，然后将超过这个范围的字符串去除。
+2. 利用加入的区间的左端点在树中进行二分查找，找到第一个左端点不小于该区间左端点的节点。
+    - 首先判断该区间和左边的区是否有交集。
+    - 然后判断和右边的若干个区间是否有交集。
+
+在扫描区间的时候，遇到有交集的区间，就需要将该区间从树上删除，并维护好对应的值。
+
+判断何时想output_发送eof，我是将第一次传入eof标志的区间算出去右端点然后存下来，之后每次处理完后就检查
+还没有发送的第一个index和eof的最后一个index是否相同，如果相同的话就说明所有数据都接收完毕了，就向output_发送eof。
 
 Implementation Challenges:
 []
